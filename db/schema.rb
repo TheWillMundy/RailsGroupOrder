88145410cebbdd48172bd44f9a212d79a62649fa
ThunderLight25 <cogsteeler2@gmail.com>
2017-04-29 13:40:28 +0800

    Added reviews, reviews link to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428165654) do
+ActiveRecord::Schema.define(version: 20170429044223) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170428165654) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "productimage"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
