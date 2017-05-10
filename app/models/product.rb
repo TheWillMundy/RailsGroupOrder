cd1fed3ae430b021ebb64acc0a61e87a6ccd2888
ThunderLight25 <cogsteeler2@gmail.com>
2017-05-10 15:19:05 +0800

    Added placeholders and presence validation for First & Last Names

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   mount_uploader :productimage, ProductImageUploader
   has_many :reviews
+  validates :name, :user, :price, :productimage, presence: true
+  validates :price, format: { with: /\A\d{1,}.\d{1,2}\z/, message: "Must be in format like so: $25.00"}
 end
